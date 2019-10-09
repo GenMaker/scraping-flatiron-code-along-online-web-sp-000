76813e94e4cbf697b8b136be7284716d3b80d96a
Panela Leung <leungpanela@gmail.com>
2019-10-09 12:27:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,20 +28,20 @@ class Scraper
       course.description = post.css("p").text
     end
   end
+
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
-Scraper.new.print_courses
-
-# def print_courses
-#   self.make_courses
-#   Course.all.each do |course|
-#     if course.title && course.title != ""
-#       puts "Title: #{course.title}"
-#       puts "  Schedule: #{course.schedule}"
-#       puts "  Description: #{course.description}"
-#     end
-#   end
-# end
-
 # Scraper.new.get_page #grabs page
 # doc.css(".post") # collection of course offerings
 # doc.css(".post").first # first course offering
